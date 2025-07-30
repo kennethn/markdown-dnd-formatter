8c07924d7d0fbc024aa01fe24cd850c57c3b4aff
Ken Norton <ken@kennethnorton.com>
2025-07-29 17:56:39 -0700

    Better footers

--- first-h1-big.lua
+++ first-h1-big.lua
@@ -1,15 +1,53 @@
+-- local seen_first_h1 = false
+
+-- function Header(el)
+--   if el.level == 1 and not seen_first_h1 then
+--     seen_first_h1 = true
+--     local content = pandoc.utils.stringify(el.content)
+--     return pandoc.RawBlock("latex", [[
+-- \begingroup
+-- \begin{center}
+-- \vspace*{-14pt}
+-- \Huge\color{sectioncolor}\headerfontbold\faDiceD20\,
+-- ]] .. content .. [[
+-- \\[-18pt]
+-- \color{sectioncolor}\rule{\linewidth}{2pt}
+-- \\[-2pt]
+-- \end{center}
+-- \endgroup
+-- ]])
+--   else
+--     return el
+--   end
+-- end
+-- Helper function to escape LaTeX specials
+
+-- Escape LaTeX special characters
+function escape_latex(str)
+  local replacements = {
+    ["\\"] = "\\textbackslash{}",
+    ["{"] = "\\{", ["}"] = "\\}",
+    ["$"] = "\\$", ["&"] = "\\&", ["%"] = "\\%",
+    ["#"] = "\\#", ["_"] = "\\_", ["^"] = "\\^{}", ["~"] = "\\~{}"
+  }
+  return (str:gsub("[\\${}&%%#_^~]", replacements))
+end
+
 local seen_first_h1 = false
 
 function Header(el)
   if el.level == 1 and not seen_first_h1 then
     seen_first_h1 = true
     local content = pandoc.utils.stringify(el.content)
+    local escaped = escape_latex(content)
+
     return pandoc.RawBlock("latex", [[
+\newcommand{\staticleftmark}{]] .. escaped .. [[}
 \begingroup
 \begin{center}
 \vspace*{-14pt}
 \Huge\color{sectioncolor}\headerfontbold\faDiceD20\,
-]] .. content .. [[
+]] .. escaped .. [[
 \\[-18pt]
 \color{sectioncolor}\rule{\linewidth}{2pt}
 \\[-2pt]
