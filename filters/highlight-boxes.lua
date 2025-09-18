7334b7d082aa6b5c8e0f6ae12d47c59ac36622aa
Ken Norton <ken@kennethnorton.com>
2025-09-18 16:24:40 -0700

    Nord-inspired color theme

--- filters/highlight-boxes.lua
+++ filters/highlight-boxes.lua
@@ -128,7 +128,7 @@ end
   -- Music callout box
   elseif el.classes:includes('musicbox') then
     return utils.create_tcolorbox(
-      'musiccolor', 'white',
+      'musiccolor', 'black',
       '\\faIcon{music}', el.content
     )
   end
